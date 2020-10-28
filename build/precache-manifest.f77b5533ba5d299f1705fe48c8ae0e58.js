e7ceea1a87db1acd0d0277f1539412e1f049348d
Hamidreza Baniasadi <hamidreza.neo@gmail.com>
2020-10-28 21:08:48 +0330

    .

---
+++ build/precache-manifest.f77b5533ba5d299f1705fe48c8ae0e58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12786e00f8978fcde2ab712a8d5f8735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ee61c907f6a82380ebc",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "0ee61c907f6a82380ebc",
+    "url": "/static/js/2.94cef731.chunk.js"
+  },
+  {
+    "revision": "3b60f97edbde0492cb8c416ad9dd7fc4",
+    "url": "/static/js/2.94cef731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "612047ca25bd28cd2b93",
+    "url": "/static/js/main.307ad170.chunk.js"
+  },
+  {
+    "revision": "04a7ce383bd0d26e9451",
+    "url": "/static/js/runtime-main.43336630.js"
+  }
+]);
